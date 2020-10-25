b58db45380b8aa7994d3f9c71dd0c8bfc3f73f45
slayenger <sinyavskiym@mail.ru>
2020-10-25 14:44:47 +0300

    Добавлены математические функции

--- main.c
+++ main.c
@@ -1,8 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+float summ(float a, float b){
+printf("Сумма чисел %f и %f равна %f\n", a, b , a+b);
+
+return a+b ;
+}
+
+float difference(float a, float b) {
+    printf("Разность чисел %f и %f равна %f\n", a, b, a-b);
+return a-b;
+}
+
+float multiplication(float a, float b) {
+    printf("Произведение чисел %f и %f равно %f\n", a, b , a*b);
+return a*b;
+}
+
+float division(float a , float b) {
+    printf("Деление чисел %f и %f равно %f\n", a, b, a/b);
+return a/b ;
+}
+
 int main()
 {
     printf("Hello world!\n");
+    summ(3, 1); //тест суммы 
+    difference(3, 1); // тест разности
+    multiplication(3, 3); // тест умножения
+    division(10, 2); // тест деления
     return 0;
 }
