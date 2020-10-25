022fa00567495ac83e3a4c6f6cf55d9f5a26a4f4
slayenger <sinyavskiym@mail.ru>
2020-10-25 14:57:31 +0300

    Добавил функцию разности чисел

--- main.c
+++ main.c
@@ -2,11 +2,16 @@
 #include <stdlib.h>
 
 float summ(float a, float b){
-printf("Сумма чисел %f и %f равна %f", a, b , a+b);
+printf("Сумма чисел %f и %f равна %f\n", a, b , a+b);
 
 return a+b ;
 }
-int main()
+
+float difference(float a, float b) {
+    printf("Разность чисел %f и %f равна %f\n", a, b, a-b);
+return a-b;
+}
+
 int main()
 {
     printf("Hello world!\n");
