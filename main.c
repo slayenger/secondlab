588b38504106caa7b18933b3848635ada421bf3c
slayenger <sinyavskiym@mail.ru>
2020-10-25 14:44:47 +0300

    Добавил функцию сложения двух чисел

--- main.c
+++ main.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+float summ(float a, float b){
+printf("Сумма чисел %f и %f равна %f", a, b , a+b);
+
+return a+b ;
+}
+int main()
 int main()
 {
     printf("Hello world!\n");
+    summ(3, 1);
     return 0;
 }
